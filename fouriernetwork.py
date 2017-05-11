19d3dea191a013de128904988d2e23756c3da73d
Maxwell Nye <maxnye@comcast.net>
2017-05-10 22:56:38 -0400

    all good in the hood. gonna mess with the training situation

--- fouriernetwork.py
+++ fouriernetwork.py
@@ -14,28 +14,27 @@ import numpy as np
 import tensorflow as tf
 from fourier_stuff import fourier_trans
 from hand_code_real_fft_network import hand_code_real_fft_network_fun
+import matplotlib.pyplot as plt
 
 # initial conditions
 complex_n = 16
 n = 2*complex_n
 logn = int(np.ceil(np.log2(complex_n)))
-train_time = 40000
+train_time = 100000
 batch_size = n #for covariance prop training
-optimizer_parameter = 0.001
-beta = 0.000000001 #needs to be dynamically adjusted???
-W_init_stddev = .2 #normalize this 
+optimizer_parameter = 0.001 #it sometimes converges at .001
+beta = 0.01 #needs to be dynamically adjusted???
+W_init_stddev = .21 #normalize this 
 loss_print_period = train_time/100
 
 
 
 # network parameters (weights)
-#W_ft_init = hand_code_real_fft_network_fun(complex_n, W_init_stddev)
+W_ft_init = hand_code_real_fft_network_fun(complex_n, W_init_stddev)
 #W = [tf.Variable(W_ft_init[i]) for i in range(len(W_ft_init))]
 
 W = [tf.Variable(tf.random_normal([n, n], stddev=W_init_stddev), dtype=tf.float32)
     for i in range(logn + 1)]
-
-
 
 
 # network layers
@@ -65,8 +64,6 @@ def l_1_norm(W):
         l1 = l1 + np.sum(abs(W[i]))
     return l1
     
-    
-
 
 # loss - do I need regularizer here?
 # regularizer = l_0_norm(W) #should this be l1 so it is convex??
@@ -86,13 +83,14 @@ train = optimizer.minimize(regularized_loss)
 
 input_train = []
 output_train = []
-
-
 for i in range(train_time):
     #input_train.append(np.random.randn(batch_size,n))
     input_train.append(np.identity(n))
     output_train.append(np.transpose(fourier_trans(input_train[i])))
     #the above line is surely fucked up in a major way
+
+
+
 
 # training loop
 init = tf.global_variables_initializer()
@@ -104,27 +102,39 @@ sess.run(init)
 #            input_vect:input_train[i],ft_output:output_train[i]
 #            })
 #    loss_val = sess.run(loss)
+
 print("optimal L1 norm: %s," %(l_1_norm(hand_code_real_fft_network_fun(complex_n,0))*beta))
+reglossvec = []
+fnlossvec = []
 for i in range(train_time):
     reg_loss_val,fn_loss_val, _ = sess.run([regularized_loss,fn_loss, train],{input_vec:input_train[i],ft_output:output_train[i]})
+    
     if i%loss_print_period == 0:
         print("step %s, function loss: %s, regularized loss: %s" 
               %(i,fn_loss_val,reg_loss_val))
+    
+    reglossvec.append(reg_loss_val)
+    fnlossvec.append(fn_loss_val)
     assert not np.isnan(fn_loss_val)
     assert not np.isnan(reg_loss_val)
     
-    
-    
-    
-    
+Wcurr = sess.run(W)    
 
-#evaluate accuracy
-#test_batch 
+plt.plot(reglossvec[100:-1])
+plt.xlabel('trial number')
+plt.xaxis(range(100,4000))
+plt.ylabel('regularized loss')
+plt.title('Regularized loss versus trial number')
+plt.show()
+
+plt.plot(fnlossvec)
+plt.xlabel('trial number')
+plt.ylabel('function squared error loss')
+plt.title('function loss versus trial number')
+plt.show()
 
 
-
-
-#curr_W = sess.run(W, {input_vec:input_train[0]})
-
-
+plt.imshow(Wcurr[1])
+plt.title('heatmap of W_1 with beta = %s'%beta)
+plt.colorbar()
 
