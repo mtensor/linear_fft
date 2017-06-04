13bd1ba2c41640c60e4153cc2b3b2e8b51ed0eba
Maxwell Nye <maxnye@comcast.net>
2017-06-04 17:36:52 -0400

    added testrunscript.sh

---
+++ odyssey/testrunscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#BATCH -J single_fouriernetwork
+#SBATCH -o single_fouriernetwork.out
+#SBATCH -e single_fourernetwork.err
+#SBATCH -N 1
+#SBATCH -n 32
+#SBATCH -t 0-40:00
+#SBATCH -p general
+#SBATCH --mem=24000
+#SBATCH --mail-type=ALL      
+#SBATCH --mail-user=mnye@college.harvard.edu
+
+module load gcc/4.9.3-fasrc01 tensorflow/0.12.0-fasrc02
+python fouriernetwork_odyssey.py
