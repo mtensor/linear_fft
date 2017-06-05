b47167d8dd2121b84109cf44f1709f7c6eb00fed
Maxwell Nye <maxnye@comcast.net>
2017-06-05 00:27:55 -0400

    made for 64

--- odyssey/testrunscript.sh
+++ odyssey/testrunscript.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 #BATCH -J single_fouriernetwork
-#SBATCH -o single_fouriernetwork32.out
-#SBATCH -e single_fouriernetwork32.err
+#SBATCH -o single_fouriernetwork64.out
+#SBATCH -e single_fouriernetwork64.err
 #SBATCH -N 1
 #SBATCH -n 32
 #SBATCH -t 0-40:00
 #SBATCH -p serial_requeue
-#SBATCH --mem=24000
+#SBATCH --mem=32000
 #SBATCH --mail-type=ALL      
 #SBATCH --mail-user=mnye@college.harvard.edu
 
 module load gcc/4.9.3-fasrc01 tensorflow/0.12.0-fasrc02
-python fouriernetwork_odyssey.py
+python fouriernetwork_odyssey646464646464.py
