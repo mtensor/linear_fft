b3c1c7f369706596a2b137720612c4a6dceb0538
Maxwell Nye <maxnye@comcast.net>
2017-06-20 19:04:03 -0400

    added boost factor

--- odyssey/launch_fouriernetwork.py
+++ odyssey/launch_fouriernetwork.py
@@ -24,9 +24,10 @@ noffsets = 4
 rseed_offsets = np.linspace(0,rseed*(noffsets-1),noffsets).astype(int)
 expt = settings.expt
 
-complexsizes = [64, 128, 256]
+complexsizes = [16, 32, 64] #[64, 128, 256]
 optimizer_params = [0.0001]
-L1_betas = [0.001, 0.0001, 0.00005]
+L1_betas = [0.0001] #[0.001, 0.0001, 0.00005]
+boost_factor = [1., 1.001, 1.01, 1.1]
 
 i = 1
 for n in complexsizes:
@@ -35,7 +36,7 @@ for n in complexsizes:
             for ws in weightscales:
                 for roff in rseed_offsets:
                     savefile = "/n/home09/mnye/linear_fft/odyssey/results/fouriernetwork/expt%d/data/res%d.npz" %(expt, i) 
-                    fo.write("-rseed %d -rseed_offset %d -weightscale %g -complexsize %d -beta %g -optimizer %g -epochs 2000000 -savefile %s -layerwise_l1\n" % (rseed, roff, ws, n, beta, optimizer, savefile))
+                    fo.write("-rseed %d -rseed_offset %d -weightscale %g -complexsize %d -beta %g -optimizer %g -epochs 2000000 -savefile %s -layerwise_l1 -boost_factor %g\n" % (rseed, roff, ws, n, beta, optimizer, savefile, boost_factor))
                     i = i+1
                     #what is lr?
                     #epoch thing may need to be cut
