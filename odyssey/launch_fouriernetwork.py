20881a3923cda24c60d747b0cb9d16d99d0c6089
Maxwell Nye <maxnye@comcast.net>
2017-06-23 15:38:39 -0400

    ready for next expt, changed launch

--- odyssey/launch_fouriernetwork.py
+++ odyssey/launch_fouriernetwork.py
@@ -20,7 +20,7 @@ os.makedirs("/n/home09/mnye/linear_fft/odyssey/results/fouriernetwork/expt%d/log
 
 weightscales = [.1]
 rseed = 2
-noffsets = 4
+noffsets = 8
 rseed_offsets = np.linspace(0,rseed*(noffsets-1),noffsets).astype(int)
 expt = settings.expt
 
@@ -28,7 +28,7 @@ complexsizes = [16, 32, 64, 128]
 optimizer_params = [0.0001]
 L1_betas = [0.0001] #[0.001, 0.0001, 0.00005]
 boost_factors = [1.]
-hidden_width_multipliers = [1., 1.2, 2.0, 5.0]
+hidden_width_multipliers = [1.0]
 
 i = 1
 for n in complexsizes:
@@ -39,7 +39,7 @@ for n in complexsizes:
                     for ws in weightscales:
                         for roff in rseed_offsets:                   
                             savefile = "/n/home09/mnye/linear_fft/odyssey/results/fouriernetwork/expt%d/data/res%d.npz" %(expt, i) 
-                            fo.write("-rseed %d -rseed_offset %d -weightscale %g -complexsize %d -beta %g -optimizer %g -epochs 4000000 -savefile %s -boost_factor %g -hidden_width_multiplier %g\n" % (rseed, roff, ws, n, beta, optimizer, savefile, boost_factor, hidden_width_multiplier))
+                            fo.write("-rseed %d -rseed_offset %d -weightscale %g -complexsize %d -beta %g -optimizer %g -epochs 2000000 -savefile %s -boost_factor %g -hidden_width_multiplier %g\n" % (rseed, roff, ws, n, beta, optimizer, savefile, boost_factor, hidden_width_multiplier))
                             i = i+1
                             #what is lr?
                             #epoch thing may need to be cut
