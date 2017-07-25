fixed averager, ready to run shit

odyssey/run_fouriernetwork_odyssey.py
@@ -17,7 +17,7 @@ parser.add_argument('-line', type=int)
 parser.add_argument('-rseed', type=int, default=1) #Keep
 parser.add_argument('-rseed_offset', type=int, default=0) #Keep
 
-parser.add_argument('-epochs',     type=int, default=100000) #Keep
+parser.add_argument('-epochs',     type=int, default=200000) #Keep
 parser.add_argument('-weightscale', type=float, default=0.21) #Keep
 parser.add_argument('-beta', type=float, default=0.00001) #0.0001
 parser.add_argument('-optimizer', type=float, default=0.0001)
@@ -94,12 +94,12 @@ for i in range(1,logn - 1): #total of logn layers
 W.append(tf.Variable(tf.random_normal([n, hidden_width], stddev=W_init_stddev), dtype=tf.float32))
 """
 
-
+"""
 ######Initialize with identity matrix######
 W = [tf.Variable(tf.eye(n) + tf.random_normal([n, n], stddev=W_init_stddev), dtype=tf.float32)
     for i in range(logn)]
 print("initialized with noisy identity matrix")
-
+"""
 
 # network layers
 input_vec = tf.placeholder(tf.float32, shape=[n,None])